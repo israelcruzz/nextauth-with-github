e374e025d17c5cc49bb84293cd0d0cc2d01c3813
israelcruzz <israelcruz.contato@gmail.com>
2024-05-16 18:11:17 -0300

    feat: add onclick event in signout button component

--- src/components/logout-button-github/index.tsx
+++ src/components/logout-button-github/index.tsx
@@ -1,8 +1,13 @@
 "use client";
 
+import { signOut } from "next-auth/react";
+
 export const LogoutButtonGithub = () => {
   return (
-    <button className="mt-8 px-4 py-3 xl:px-6 xl:py-5 gap-6 bg-[#1E1E1E] text-white flex items-center justify-center rounded-lg text-xs xl:text-2xl hover:bg-[#1e1e1eee]">
+    <button
+      className="mt-8 px-4 py-3 xl:px-6 xl:py-5 gap-6 bg-[#1E1E1E] text-white flex items-center justify-center rounded-lg text-xs xl:text-2xl hover:bg-[#1e1e1eee]"
+      onClick={() => signOut()}
+    >
       Logout
     </button>
   );
