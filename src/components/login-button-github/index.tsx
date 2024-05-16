f1b9c9db6b661777419779c545a109a22282a720
israelcruzz <israelcruz.contato@gmail.com>
2024-05-16 17:43:29 -0300

    feat: add event click for signIn with github

--- src/components/login-button-github/index.tsx
+++ src/components/login-button-github/index.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import Image from "next/image";
+import { signIn } from 'next-auth/react'
 
 export const LoginButtonGithub = () => {
   return (
-    <button className="mt-8 px-4 py-3 xl:px-6 xl:py-5 gap-6 bg-[#1E1E1E] text-white flex items-center justify-center rounded-lg text-xs xl:text-2xl hover:bg-[#1e1e1eee]">
+    <button className="mt-8 px-4 py-3 xl:px-6 xl:py-5 gap-6 bg-[#1E1E1E] text-white flex items-center justify-center rounded-lg text-xs xl:text-2xl hover:bg-[#1e1e1eee]" onClick={() => signIn()}>
       <Image
         src="/github-icon.svg"
         width={38}
