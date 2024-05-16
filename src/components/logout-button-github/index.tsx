710674864ce291d23b324b0dee417723dd2d55b9
israelcruzz <israelcruz.contato@gmail.com>
2024-05-15 21:24:50 -0300

    feat: add logout button component was created

---
+++ src/components/logout-button-github/index.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const LogoutButtonGithub = () => {
+  return (
+    <button className="mt-8 px-4 py-3 xl:px-6 xl:py-5 gap-6 bg-[#1E1E1E] text-white flex items-center justify-center rounded-lg text-xs xl:text-2xl hover:bg-[#1e1e1eee]">
+      Logout
+    </button>
+  );
+};
