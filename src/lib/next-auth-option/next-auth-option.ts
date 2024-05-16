039824fcffc586751ed7da2cac2a2d1db02fd531
israelcruzz <israelcruz.contato@gmail.com>
2024-05-16 17:41:46 -0300

    feat: add env in next auth config file

--- src/lib/next-auth-option/next-auth-option.ts
+++ src/lib/next-auth-option/next-auth-option.ts
@@ -1,11 +1,12 @@
+import { env } from "@/services/env/env";
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
 export const nextAuthConfig: NextAuthOptions = {
   providers: [
     GithubProvider({
-      clientId: "",
-      clientSecret: "",
+      clientId: env.data?.GITHUB_ID as string,
+      clientSecret: env.data?.GITHUB_SECRET as string,
     }),
   ],
 };
